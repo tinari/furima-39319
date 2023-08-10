618f83db42420da681501e9370707523900c7197
tinari <kurisumasunama@yahoo.co.jp>
2023-08-10 17:11:58 +0900

    Revert "商品詳細表示機能①"
    This reverts commit 6de91040644fecd156bfa2610701e12a96425cc1.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index]
+  before_action :move_to_index, except: [:index]
 
   def index
     @items= Item.all.order(created_at: :desc)
@@ -16,6 +16,7 @@ class ItemsController < ApplicationController
       if @item.save
         redirect_to root_path 
       else
+        
         render :new, locals: { item: @item }
         
 
@@ -23,9 +24,8 @@ class ItemsController < ApplicationController
     end
 
   def show
-    @item= Item.find(params[:id])
+   # @item= Item.find(params[:id])
   end
-
 
 
   private
