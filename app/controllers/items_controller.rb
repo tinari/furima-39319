b5b835c6303646801ddca7c795b03ab08796bd22
tinari <kurisumasunama@yahoo.co.jp>
2023-08-11 14:49:31 +0900

    情報編集機能②

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, except: [:index, :show]
+  before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @items= Item.all.order(created_at: :desc)
@@ -14,7 +15,7 @@ class ItemsController < ApplicationController
   def create
       @item= Item.new(item_params)
       @item.image.attach(params[:item][:image]) if params[:item][:image]
-      if @item.update
+      if @item.save
         redirect_to root_path 
       else
         render :new, locals: { item: @item }
@@ -23,15 +24,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item= Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
   
   def update
-    @item = Item.find(params[:id])
+  
     
     if @item.update(item_params)
       redirect_to item_path(@item)
@@ -51,5 +50,9 @@ class ItemsController < ApplicationController
 
   def move_to_index
     redirect_to root_path unless user_signed_in?
+  end
+
+  def set_item
+    @item= Item.find(params[:id])
   end
 end
